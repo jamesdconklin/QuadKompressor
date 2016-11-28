fe76af9bf2ba303e3bfc884fde121739a824d526
Michael Loschiavo <losch103@gmail.com>
2016-11-27 21:38:11 -0800

    Add QT node file

---
+++ frontend/components/showcase/QTNode.js
@@ -0,0 +1,91 @@
+const THRESHOLD = 500;
+
+class QTNode {
+  constructor(pixels, x, y, dim) {
+    this.x = x;
+    this.y = y;
+    this.dim = dim;
+
+    this.color = null;
+    // idx 0 -> NW. Increases clockwise.
+    this.children = [];
+
+    let { average, variance } = this.chromaticAnalysis(pixels, x, y, dim);
+
+    if ( variance > THRESHOLD) {
+      this.children.push(new QTNode(pixels, x, y, dim/2));
+      this.children.push(new QTNode(pixels, x+dim/2, y, dim/2));
+      this.children.push(new QTNode(pixels, x+dim/2, y+dim/2, dim/2));
+      this.children.push(new QTNode(pixels, x, y+dim/2, dim/2));
+    } else {
+      this.color = average;
+    }
+
+    this.promiseActions(average, variance, pixels, x, y, dim).then(result => {
+      console.log("result: ", result)
+    }, (errMessage) => {
+      console.log('Error, ', errMessage)
+    });
+
+  }
+
+  promiseActions(average, variance, pixels, x, y, dim) {
+    console.log('promiseActions is called')
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if ( variance > THRESHOLD) {
+          resolve(`logging for x: ${x} y: ${y}`)
+        } else {
+          reject(`this hit for the break case or an error`)
+        }
+      }, 300);
+    });
+  }
+
+  chromaticDifference(v1, v2) {
+    let redDiff = v1.red-v2.red,
+        greenDiff = v1.green-v2.green,
+        blueDiff = v1.blue-v2.blue;
+
+    let diff = redDiff + greenDiff + blueDiff;
+    console.log()
+    return diff;
+  }
+
+  chromaticAnalysis(pixels, x0, y0, dim) {
+    let redBar = 0, greenBar = 0, blueBar = 0;
+
+    for (var x = x0; x < x0+dim; x++) {
+      for (var y = y0; y < y0+dim; y++) {
+        let { red, green, blue } = pixels[x][y];
+        redBar += red; greenBar += green, blueBar += blue;
+      }
+    }
+    let count = Math.pow(dim, 2);
+    redBar /= count;
+    greenBar /= count;
+    blueBar /= count;
+
+    let colorBar = {
+      red: redBar,
+      green: greenBar,
+      blue: blueBar
+    };
+    debugger
+    let summedSquares = 0;
+
+    for (var x = x0; x < x0+dim; x++) {
+      for (var y = y0; y < y0+dim; y++) {
+        summedSquares += this.chromaticDifference(colorBar, pixels[x][y]);
+      }
+    }
+
+    return {
+      average: colorBar,
+      variance: summedSquares / count
+    };
+
+  }
+}
+
+export default QTNode;
