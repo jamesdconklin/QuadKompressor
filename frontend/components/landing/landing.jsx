e050955fd5c482d77d88061c4fa2cec37c1c26bb
Michael Loren Loschiavo <losch103@gmail.com>
2016-11-30 16:29:47 -0800

    Change -sm to -lg

--- frontend/components/landing/landing.jsx
+++ frontend/components/landing/landing.jsx
@@ -14,7 +14,7 @@ class Landing extends React.Component {
           </div>
         </div>
         <div className="row">
-          <div className="col-sm-6">
+          <div className="col-lg-6">
             <p>
               QT Kompressor introduces users to quadtrees by demonstrating their use
               in breaking down and reconstructing two dimensional data, in this case
@@ -22,16 +22,16 @@ class Landing extends React.Component {
             </p>
             <button onClick={()=>hashHistory.push("/showcase")}>Open the Gallery</button>
           </div>
-          <div className="col-sm-6">
+          <div className="col-lg-6">
             <CloudinaryImage publicId="qi8iq3nfxcdggkkrgi6u" options={{width: 300, height: 300, crop: "scale"}}/>
           </div>
         </div>
         <div className="row">
-          <div className="col-sm-6">
+          <div className="col-lg-6">
             <CloudinaryImage publicId="zssaahlczsmqc4yfazzk" options={{width: 300, height: 180 , crop: "scale"}}/>
             <div>H/T <a href="http://blog.notdot.net/2009/11/Damn-Cool-Algorithms-Spatial-indexing-with-Quadtrees-and-Hilbert-Curves">Nick Johnsonz</a></div>
           </div>
-          <div className="col-sm-6">
+          <div className="col-lg-6">
             <p>
               Quadtrees, as the name suggests, are trees whose nodes have either zero
               children or four.
@@ -39,7 +39,7 @@ class Landing extends React.Component {
           </div>
         </div>
         <div className="row">
-          <div className="col-sm-6">
+          <div className="col-lg-6">
             <p>
               To build a quadtree, we first insert a root node covering the entirety
               of a given two dimensional array of values. If that region can be construed
@@ -50,12 +50,12 @@ class Landing extends React.Component {
               process continues until all branches end in leaves.
             </p>
           </div>
-          <div className="col-sm-6">
+          <div className="col-lg-6">
             <CloudinaryImage publicId="qsizaefwwcnuuchslv1k" options={{width: 256, height: 256 , crop: "scale"}}/>
           </div>
         </div>
         <div className="row">
-          <div className="col-sm-12">
+          <div className="col-lg-12">
             <p>
               When applying this structure to an image for compression, we need to decide
               if a given region, and thus a given node, is single-valued. We accomplished
